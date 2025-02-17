01b411f79bb1b6c11c6eabf85593a977cf7293fc
Mihovil Čeperić <mihovil.ceperic@ericssonnikolatesla.com>
2025-02-17 15:52:36 +0100

    Removed unnecessary files

--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE sensor_reading (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    sensor_type VARCHAR(255) NOT NULL,
-    sensor_value DOUBLE NOT NULL,
-    timestamp TIMESTAMP NOT NULL
-);
