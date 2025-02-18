36b118b40e0ba572ef38a2d722a6dd2eb3543912
Mihovil Čeperić <mihovil.ceperic@ericssonnikolatesla.com>
2025-02-18 13:10:59 +0100

    Add chart.js but needs fix to make it work

--- frontend/airqualityapp/src/app/sensor-list/sensor-list.component.ts
+++ frontend/airqualityapp/src/app/sensor-list/sensor-list.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { SensorService } from '../sensor.service';
 import { SensorReading } from '../models/sensor-reading.model';
+import { Chart, registerables, TimeScale } from 'chart.js';
+import * as dayjs from 'dayjs';
+
+Chart.register(...registerables);
+Chart.register(TimeScale);
+
+Chart.defaults.set('time', {
+  adapter: dayjs
+});
 
 @Component({
   selector: 'app-sensor-list',
@@ -11,6 +20,7 @@ import { SensorReading } from '../models/sensor-reading.model';
 export class SensorListComponent implements OnInit{
 
   sensorReadings: SensorReading[] = [];
+  chart: any;
 
   constructor(private sensorService: SensorService) {}
 
@@ -18,10 +28,71 @@ export class SensorListComponent implements OnInit{
     this.sensorService.getReadings().subscribe(
       (data) => {
         this.sensorReadings = data;
+        this.createChart();
       },
       (error) => {
         console.error('Error fetching sensor readings:', error);
       }
     );
+  }
+
+  createChart(): void{
+    if(!this.sensorReadings || this.sensorReadings.length === 0){
+      console.log('No data avalible to create chart.');
+      return;
+    }
+
+    const timestamps = this.sensorReadings.map((entry: any) => entry.timestamp);
+    const co2Values = this.sensorReadings.filter((entry: any) => entry.sensorType === 'CO2').map((entry: any) => entry.sensorValue);
+    const pm25Values = this.sensorReadings.filter((entry: any) => entry.sensorType === 'PM2.5').map((entry: any) => entry.sensorValue);
+    const pm10Values = this.sensorReadings.filter((entry: any) => entry.sensorType === 'PM10').map((entry: any) => entry.sensorValue);
+
+    this.chart = new Chart('sensorChart', {
+      type: 'line',
+      data: {
+        labels: timestamps,
+        datasets: [
+          {
+            label: 'CO2 Levels',
+            data: co2Values,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            fill: false
+          },
+          {
+            label: 'PM2.5 Levels',
+            data: pm25Values,
+            borderColor: 'rgba(54, 162, 235, 1)',
+            fill: false
+          },
+          {
+            label: 'PM10 Levels',
+            data: pm10Values,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        scales: {
+          x: {
+            type: 'time',
+            time: {
+              unit: 'minute'
+            },
+            title:  {
+              display: true,
+              text: 'Timestamp'
+            }
+          },
+          y: {
+            beginAtZero: true,
+            title: {
+              display: true,
+              text: 'Sensor Value'
+            }
+          }
+        }
+      }
+    });
   }
 }
