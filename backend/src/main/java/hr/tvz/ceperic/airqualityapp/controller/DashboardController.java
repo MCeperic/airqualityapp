acc6eecc44e40212cd675ae0f669ba65b67d8018
Mihovil <mceperic@gmail.com>
2025-08-24 12:35:01 +0200

    Remove old controller logic, add new

--- backend/src/main/java/hr/tvz/ceperic/airqualityapp/controller/DashboardController.java
+++ backend/src/main/java/hr/tvz/ceperic/airqualityapp/controller/DashboardController.java
@@ -1,22 +1,26 @@
 package hr.tvz.ceperic.airqualityapp.controller;
 
-import hr.tvz.ceperic.airqualityapp.dto.SensorTrendDTO;
+import hr.tvz.ceperic.airqualityapp.dto.LatestSensorReadingsDto;
 import hr.tvz.ceperic.airqualityapp.service.DashboardService;
+import lombok.AllArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/aqa/dashboard")
 public class DashboardController {
 
-    private final DashboardService service;
+    private final DashboardService dashboardService;
 
-    public DashboardController(DashboardService service) {
-        this.service = service;
+    @GetMapping("/latestReadings")
+    public ResponseEntity<LatestSensorReadingsDto> getLatestReadings(){
+        return ResponseEntity.ok(dashboardService.getLatestReadings());
     }
 
 }
